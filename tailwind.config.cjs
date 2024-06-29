a96c8eb3f3c65063713078bfe89c23d154b8a9b4
brdacostac <brunocunha1506@gmail.com>
2024-06-29 17:36:43 +0200

    feat: hero section with

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,7 +19,8 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('/src/assets/herobg.png')",
+        //Ajouter une image de fond si j'ai besoin
+        //"hero-pattern": "url('/src/assets/herobg.png')",
       },
     },
   },
