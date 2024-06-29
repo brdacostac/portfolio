a128eacc970f2b558508ac457ac69ad3d386bd7f
brdacostac <brunocunha1506@gmail.com>
2024-06-29 16:43:15 +0200

    feat: navBar finish

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter } from "react-router-dom";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
-
 const App = () => {
   return (
     <BrowserRouter>
@@ -22,7 +21,7 @@ const App = () => {
         </div>
       </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
